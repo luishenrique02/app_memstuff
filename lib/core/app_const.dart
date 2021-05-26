47e54a86b316abc0770124a4167da898216de4ae
Luís Henrique <luishenri2002@hotmail.com>
2021-05-25 23:42:31 -0300

    app finish

---
+++ lib/core/app_const.dart
@@ -0,0 +1,19 @@
+const kAppTitle = 'MemStuff_app';
+
+const kTitleNewLoan = 'empréstimo novo';
+const kTitleDetails = 'Detalhes';
+const kLabelName = 'Nome';
+const kLabelDescription = 'Descrição';
+const kLabelPhone = 'Telefone';
+const kLabelLoanDate = 'Data do empréstimo';
+const kButtonClean = 'Limpar';
+const kButtonSave = 'Salvar';
+
+const kDatabaseName = 'MemStuff_app.db';
+const kDatabaseVersion = 1;
+
+const kButtonPadding = 8.0;
+const kButtonHeight = 46.0;
+const kButtonRadius = 11.0;
+const kButtonFontSize = 21.0;
+const kButtonLetterSpacing = 1.00;
